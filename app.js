1e3d0ff087645a62d8cfbe003486d503663995fe
Zijian Liu <liu.zijian@outlook.com>
2020-11-03 10:36:51 +0000

    HTTPS

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 const controller = require('./controller');
+const static = require('koa-static');
+const https = require('https');
+const fs = require('fs');
 const spotify_chart = require('./spotify/spotify_chart');
 
 
@@ -18,6 +21,11 @@ app.use(bodyParser());
 // register controllers
 app.use(controller());
 
+
+// for certbot
+// app.use(static('./letsencrypt', {hidden: true})); // server static files
+// app.listen(80);
+
 const update_spotify_charts = async () => {
     await spotify_chart.fetch_regions_periodic();
     process.env.NODE_ENV === 'production' && await spotify_chart.fetch_charts_periodic();
@@ -27,15 +35,16 @@ const update_spotify_charts = async () => {
 
 update_spotify_charts();
 
-app.listen(3000);
+// SSL for HTTPS
+fs.copyFileSync('/etc/letsencrypt/live/spotify.zijian.xyz/privkey.pem', './letsencrypt/privkey.pem');
+fs.copyFileSync('/etc/letsencrypt/live/spotify.zijian.xyz/cert.pem', './letsencrypt/cert.pem');
+fs.copyFileSync('/etc/letsencrypt/live/spotify.zijian.xyz/chain.pem', './letsencrypt/chain.pem');
 
-// for HTTPS
-// const https = require('https');
-// const fs = require('fs');
-// const options = {
-//     key: fs.readFileSync('key.pem'),
-//     cert: fs.readFileSync('cert.pem')
-// };
-//
-// https.createServer(options, app.callback()).listen(3000);
+const options = {
+    key: fs.readFileSync('./letsencrypt/privkey.pem'),
+    cert: fs.readFileSync('./letsencrypt/cert.pem'),
+    ca: fs.readFileSync('./letsencrypt/chain.pem')
+};
+
+https.createServer(options, app.callback()).listen(3000);
 console.log('listening at port 3000...');

35986f37d85234087f2587dab0b0a6eb78f48977
Zijian Liu <liu.zijian@outlook.com>
2020-11-21 00:27:59 -0500

    daily rotate logging

--- logs/logger.js
+++ logs/logger.js
@@ -1,24 +1,34 @@
 const koa_logger = require('koa-logger');
 const stripAnsi = require('strip-ansi');
 const {createLogger, format, transports} = require('winston');
+require('winston-daily-rotate-file');
 const fs = require('fs');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+const formatDate = (date) => {
+    // YYYY-MM-DD
+    return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`;
+}
+
 const winston_logger = createLogger({
     level: 'info',
     format: format.combine(
-        format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
+        format.timestamp(),
         format.errors({stack: true}),
         format.splat(),
         format.json()
     ),
     // defaultMeta: {service: 'spotify-charts-generator-server'},
     transports: [
-        new transports.File({filename: './logs/winston-error.log', level: 'error'}),
-        new transports.File({filename: './logs/winston-combined.log'})
+        new transports.File({filename: './logs/winston_logs/winston-errors.log', level: 'error'}),
+        new transports.DailyRotateFile({
+            filename: 'winston-%DATE%.log',
+            dirname: './logs/winston_logs',
+            datePattern: 'YYYY-MM-DD',
+            maxFiles: '14d',
+            utc: true
+        })
     ]
 });
 
@@ -28,8 +38,9 @@ module.exports.koa_logger = koa_logger(({
     transporter: (str, args) => {
         !isProduction && console.log(str);
         // using strip-ansi to remove chalk style from the string
-        const s = `${new Date().toUTCString()}: ${stripAnsi(str)}\n`;
-        fs.writeFile('./logs/http_req_res.log', s, {flag: 'a'}, err => {
+        const date = new Date();
+        const s = `${date.toISOString()}: ${stripAnsi(str)}\n`;
+        fs.writeFile(`./logs/koa_http_logs/koa-${formatDate(date)}.log`, s, {flag: 'a'}, err => {
             if (err) {
                 console.log(err);
             }

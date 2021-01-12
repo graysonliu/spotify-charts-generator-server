cfaf6c8301f8234b57a7523c80a6463d0cdb3c94
Zijian Liu <liu.zijian@outlook.com>
2021-01-12 22:48:44 +0000

    cron for ssl renew

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,1 +1,8 @@
-certbot renew --standalone --force-renewal
+certbot renew --standalone
+
+# renew ignoring whether it will be due in 30 days
+# certbot renew --standalone --force-renewal
+
+# renew monthly using cron
+# crontab -e
+# 0 0 1 * * /root/ws/spotify-charts-generator-server/ssl_renew.sh

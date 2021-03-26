af23d38a86337f8db696b9630984e43672d9ad47
Zijian Liu <liu.zijian@outlook.com>
2021-03-25 23:24:56 -0400

    :running:

--- start-prod-docker-compose.sh
+++ start-prod-docker-compose.sh
@@ -2,7 +2,5 @@ set -a # export environment variables for subprocess
 source ./env/prod-docker-compose.env
 set +a # disable
 
-# redirect output
-exec > run.log
 # run containers
-docker-compose up --no-color
+docker-compose up
